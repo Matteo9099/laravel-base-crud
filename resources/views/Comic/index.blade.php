df766a9f1cee5e931f33d641dc767c65af436b94
Matteo Morganti <matteo99morganti@gmail.com>
2022-04-01 17:44:55 +0200

    Commit

---
+++ resources/views/Comic/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.base')
+
+@section('pageTitle', 'Comic')
+
+@section('content')
+
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Immagine</th>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Serie</th>
+                    <th scope="col">Prezzo</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($comics as $comic)
+                    <tr>
+                        <td><img src="{{$comic->thumb}}" alt="{{$comic->title}}"></td>
+                        <td>{{$comic->title}}</td>
+                        <td>{{$comic->series}}</td>
+                        <td>{{$comic->price}}</td>
+                    </tr>
+                @endforeach
+                
+            </tbody>
+        </table>
+    </div>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('prova');
+    return view('home');
 });
 
 Route::resource('comic', "ComicController");
