2304b163bf9f7db2eecf4c4670c5157b1bfd2ea6
Matteo Morganti <matteo99morganti@gmail.com>
2022-04-04 18:16:40 +0200

    Aggiunto conferma cancellazione

--- resources/views/Comic/index.blade.php
+++ resources/views/Comic/index.blade.php
@@ -8,29 +8,34 @@
 
     <section class="bg-light">
         <div class="text-center">
-            <a href="{{route('comic.create')}}" class="btn btn-success text-white mt-2">CREA NUOVO FUMETTO</a>
+            <a href="{{ route('comic.create') }}" class="btn btn-success text-white mt-2">CREA NUOVO FUMETTO</a>
         </div>
         <div class="container d-flex flex-wrap">
             @foreach ($comics as $comic)
                 {{-- Info card --}}
                 <div class="card ms_card bg-dark text-white">
-                    <img src="{{$comic->thumb}}" class="card-img-top" alt="{{$comic->title}}">
+                    <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
                     <div class="card-body">
 
-                        <h5 class="card-title">{{$comic->title}}</h5>
-                        <p class="card-text">{{$comic->series}}</p>
-                        <p class="card-text">Price: <span class="text-success">{{$comic->price}} &euro;</span></p>
+                        <h5 class="card-title">{{ $comic->title }}</h5>
+                        <p class="card-text">{{ $comic->series }}</p>
+                        <p class="card-text">Price: <span class="text-success">{{ $comic->price }} &euro;</span></p>
 
                         {{-- Bottoni card --}}
                         <div class="d-flex justify-content-around">
 
-                            <a href="{{route('comic.show',  $comic->id )}}" title="Dettagli" class="btn btn-info d-flex align-items-center"><i class="fa-solid fa-eye"></i></a>                       
-                            <a href="{{route('comic.edit',  $comic->id )}}" title="Modifica" class="btn btn-warning d-flex align-items-center"><i class="fa-solid fa-pencil"></i></a>   
-    
-                            <form action="{{ route('comic.destroy', $comic->id) }}" method="POST" data-name="{{ $comic->title }}" class="form-delete">
+                            <a href="{{ route('comic.show', $comic->id) }}" title="Dettagli"
+                                class="btn btn-info d-flex align-items-center"><i class="fa-solid fa-eye"></i></a>
+                            <a href="{{ route('comic.edit', $comic->id) }}" title="Modifica"
+                                class="btn btn-warning d-flex align-items-center"><i class="fa-solid fa-pencil"></i></a>
+
+                            <form action="{{ route('comic.destroy', $comic->id) }}" method="POST"
+                                data-name="{{ $comic->title }}" class="form-delete">
                                 @csrf
                                 @method('DELETE')
-                                <button title="Elimina"class="btn btn-danger d-flex align-items-center" type="submit"><i class="fa-solid fa-trash"></i></button>
+                                <button title="Elimina" class="btn btn-danger d-flex align-items-center"
+                                    {{-- onclick="return confirm('Sicuro di voler eliminare definitivamente questo elemento?')" --}}
+                                    type="submit"><i class="fa-solid fa-trash"></i></button>
                             </form>
                         </div>
 
@@ -40,5 +45,20 @@
         </div>
     </section>
 
-@endsection
+    <script>
+        var form_delete = document.querySelectorAll('.form-delete');
 
+        for (var i = 0; i < form_delete.length; i++) {
+            form_delete[i].addEventListener('submit', function(event) {
+                event.preventDefault();
+
+                var accept = confirm('Sicuro di voler eliminare definitivamente questo elemento');
+
+                if (accept) {
+                    window.location.form_delete = event.target.submit();
+                }
+            });
+        }
+    </script>
+
+@endsection
